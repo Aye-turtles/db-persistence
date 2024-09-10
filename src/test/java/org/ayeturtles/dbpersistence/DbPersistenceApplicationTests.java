7ed9635f3b41ad993d059429fdfef1a125f2794e
Josue Aguilar <josueaguilarjnai@gmail.com>
2024-09-09 21:03:38 -0600

    first commit

---
+++ src/test/java/org/ayeturtles/dbpersistence/DbPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ayeturtles.dbpersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
