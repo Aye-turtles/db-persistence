30227b1a21099faf616d9b96acc625329ac0cb19
Angel Lara <Lara.angel.js@gmail.com>
2024-09-16 13:42:01 -0600

    feat: add mapstruct and spring boot web starter

---
+++ src/main/java/org/ayeturtles/dbpersistence/mapper/NetsMapper.java
@@ -0,0 +1,12 @@
+package org.ayeturtles.dbpersistence.mapper;
+
+import org.ayeturtles.dbpersistence.dto.Nests;
+import org.ayeturtles.dbpersistence.entities.nests.NestsReq;
+import org.ayeturtles.dbpersistence.entities.nests.NestsRes;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface NetsMapper {
+    Nests toDto(NestsReq request);
+    NestsRes toRes(Nests response);
+}
